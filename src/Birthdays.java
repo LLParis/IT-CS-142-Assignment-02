1c09490066d4c6abf72b584ef4949af0495f7688
Your Name <you@example.com>
2024-02-16 22:17:05 -0800

    Initial Commit

---
+++ src/Birthdays.java
@@ -0,0 +1,14 @@
+public class Birthdays {
+    public static void main(String[] args) {
+        // Here are some lines that might save you a bit of time as you write your code:
+        System.out.println("This program compares two birthdays");
+        System.out.println("and displays which one is sooner.");
+        // System.out.println("Today is //2020, day # of the year.");
+
+        System.out.println("Person 1:");
+        System.out.print("What month and day were you born? "); // 10 17
+        // 10/17/2020 falls on day #291 of 366.
+        // Your next birthday is in 262 day(s).
+        // That is 71.6 percent of a year away.
+    }
+}
